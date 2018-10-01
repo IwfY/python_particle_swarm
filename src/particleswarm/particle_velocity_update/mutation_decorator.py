e6e567f49bea91bfc8e5aba8c31b961ac5e2531a
Marcel Pfeiffer <m.pfeiffer@strucnamics.de>
2018-10-01 10:45:47 +0200

    particle velocity mutator: make number of mutations configurable

--- src/particleswarm/particle_velocity_update/mutation_decorator.py
+++ src/particleswarm/particle_velocity_update/mutation_decorator.py
@@ -5,10 +5,11 @@ class MutationDecorator:
 	Decorator for particle velocity update strategy that multiplies one random velocity vector if velocity length is beneath a
 	given threshold
 	'''
-	def __init__(self, particleVelocityUpdateStrategy, velocityMutationThreshold, mutationFactor = 100):
+	def __init__(self, particleVelocityUpdateStrategy, velocityMutationThreshold, mutationFactor = 100, mutationNumber = 1):
 		self.__particleVelocityUpdateStrategy = particleVelocityUpdateStrategy
 		self.__velocityMutationThresholdPow2 = pow(velocityMutationThreshold, 2)
 		self.__mutationFactor = mutationFactor
+		self.__mutationNumber = mutationNumber
 
 	def setVelocityMutationThreshold(self, velocityMutationThreshold):
 		self.__velocityMutationThresholdPow2 = pow(velocityMutationThreshold, 2)
@@ -19,8 +20,9 @@ class MutationDecorator:
 		lengthPow2 = sum(map(lambda x: x*x, newVelocity.values()))
 
 		if lengthPow2 < self.__velocityMutationThresholdPow2:
-			keyToMutate = random.choice([i for i in newVelocity.keys()])
-			#print("Mutate: {}, {}, {}, {}".format(particle.getId(), keyToMutate, newVelocity[keyToMutate], newVelocity[keyToMutate] * self.__mutationFactor))
-			newVelocity[keyToMutate] *= self.__mutationFactor
+			for c in range(0, self.__mutationNumber):
+				keyToMutate = random.choice([i for i in newVelocity.keys()])
+				#print("Mutate: {}, {}, {}, {}".format(particle.getId(), keyToMutate, newVelocity[keyToMutate], newVelocity[keyToMutate] * self.__mutationFactor))
+				newVelocity[keyToMutate] *= self.__mutationFactor
 
 		return newVelocity
